f410ca0e0561c4ea9e4876d48e80d3670f0579e4
pc_win10 <702649194@qq.com>
2026-01-03 16:44:59 +0800

    # update correct()

--- filter_utils/src/kf.cpp
+++ filter_utils/src/kf.cpp
@@ -21,11 +21,35 @@ void KF::correct() {
 	double gyro_y = state_(StateMemberGy);
 	double gyro_z = state_(StateMemberGz);
 	
+	// ×ËÌ¬±íÊ¾Ðý×ªË³Ðòyaw->pitch->roll z->y->x
 	double cos_roll = std::cos(roll);
 	double cos_pitch = std::cos(pitch);
 	double cos_yaw = std::cos(yaw);
+	double sin_roll = std::sin(roll);
+	double sin_pitch = std::sin(pitch);
+	double sin_yaw = std::sin(yaw);
 
 	transfer_func_(StateMemberX, StateMemberVx) = cos_yaw * cos_pitch * delta_t;
-	transfer_func_(StateMemberX, StateMemberVy) = 
+	transfer_func_(StateMemberX, StateMemberVy) = (-1.0 * sin_yaw * cos_roll + cos_yaw * sin_pitch * sin_roll) * delta_t;
+	transfer_func_(StateMemberX, StateMemberVz) = (sin_yaw * sin_roll + cos_yaw * sin_pitch * cos_roll) * delta_t;
+	transfer_func_(StateMemberX, StateMemberAx) = 0.5 * transfer_func_(StateMemberX, StateMemberVx) * delta_t; // 0.5*Ax*t^2
+	transfer_func_(StateMemberX, StateMemberAy) = 0.5 * transfer_func_(StateMemberX, StateMemberVy) * delta_t;
+	transfer_func_(StateMemberX, StateMemberAz) = 0.5 * transfer_func_(StateMemberX, StateMemberVz) * delta_t;
 
+	transfer_func_(StateMemberY, StateMemberVx) = sin_yaw * cos_pitch * delta_t;
+	transfer_func_(StateMemberY, StateMemberVy) = (cos_yaw * cos_roll + sin_yaw * sin_pitch * sin_roll) * delta_t;
+	transfer_func_(StateMemberY, StateMemberVz) = (-1.0 * cos_yaw * sin_roll + sin_yaw * sin_pitch * cos_roll) * delta_t;
+	transfer_func_(StateMemberY, StateMemberAx) = 0.5 * transfer_func_(StateMemberY, StateMemberVx) * delta_t;
+	transfer_func_(StateMemberY, StateMemberAy) = 0.5 * transfer_func_(StateMemberY, StateMemberVy) * delta_t;
+	transfer_func_(StateMemberY, StateMemberAz) = 0.5 * transfer_func_(StateMemberY, StateMemberVz) * delta_t;
+
+	transfer_func_(StateMemberZ, StateMemberVx) = (-1.0 * sin_pitch) * delta_t;
+	transfer_func_(StateMemberZ, StateMemberVy) = (cos_pitch * sin_roll) * delta_t;
+	transfer_func_(StateMemberZ, StateMemberVz) = (cos_pitch * cos_roll) * delta_t;
+	transfer_func_(StateMemberZ, StateMemberAx) = 0.5 * transfer_func_(StateMemberZ, StateMemberVx) * delta_t;
+	transfer_func_(StateMemberZ, StateMemberAy) = 0.5 * transfer_func_(StateMemberZ, StateMemberVy) * delta_t;
+	transfer_func_(StateMemberZ, StateMemberAz) = 0.5 * transfer_func_(StateMemberZ, StateMemberVz) * delta_t;
+
+	transfer_func_(StateMemberRoll, StateMemberGx) = delta_t;
+	transfer_func_(StateMemberRoll, StateMemberGy) = 
 }
